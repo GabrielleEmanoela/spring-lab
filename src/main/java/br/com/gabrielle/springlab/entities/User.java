b9f25c047b97ab9004351494ef9dba512fa25462
Gabrielle <biela.a09@gmail.com>
2021-03-05 01:52:45 -0300

    H2 database, test profile, JPA

--- src/main/java/br/com/gabrielle/springlab/entities/User.java
+++ src/main/java/br/com/gabrielle/springlab/entities/User.java
@@ -1,10 +1,17 @@
 package br.com.gabrielle.springlab.entities;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class User implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String nome;
     private String email;
